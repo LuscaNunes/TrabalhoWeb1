021b3ce0788f7780083427e5806ecb55cf9356d2
Lucas da Silva Nunes <lucas.silvaifro@gmail.com>
2025-01-14 22:58:07 -0400

    Incio de projeto Agape

---
+++ js/Acesso.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.getElementById('dropdownMenu');
+    if (menu.style.display === 'block') {
+      menu.style.display = 'none'; // Esconde o menu
+    } else {
+      menu.style.display = 'block'; // Mostra o menu
+    }
+  }
+  
