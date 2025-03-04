2755f376a9ae0d3a9d4b81977e69ec913ad53a7b
Lucas da Silva Nunes <lucas.silvaifro@gmail.com>
2025-03-04 00:02:42 -0400

    ultima atualização

--- js/Acesso.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById('dropdownMenu');
-    if (menu.style.display === 'block') {
-      menu.style.display = 'none'; // Esconde o menu
-    } else {
-      menu.style.display = 'block'; // Mostra o menu
-    }
-  }
-  
